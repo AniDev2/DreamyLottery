c9240f0731576f782567c9aa47349b3bce0fc564
Anna Kurek <145387158+AniDev2@users.noreply.github.com>
2023-10-17 22:02:02 +0200

    Comment Removal: Cleaning up the code.

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-//* Tablica, która będzie przechowywała nasze wyniki - żeby nie wypadły dwie te same liczby *//
 const result = [];
 const circles = document.getElementById("circles");
 
-//* Tworzymy funkcję z elementem div (zamiast w html) - tym, który jest ostylowany w css (niebieskie kółko) *//
-
 function losowanie() {
-  if (result.length === 6) return; //* Po sześciu wylosowanych divach przestanie losować *//
-  const wyniklosowania = Math.floor(Math.random() * 49 + 1); //* "+ 1" niweluje to, że otrzymamy 0 *//
+  if (result.length === 6) return;
+  const wyniklosowania = Math.floor(Math.random() * 49 + 1);
   for (let i = 0; i < result.length; i++) {
     if (wyniklosowania === result[i]) {
       return losowanie();
     }
-  } //* pętla - żeby za każdym razem jak zostanie wylosowana liczba js sprawdził w tablicy, czy taka liczba już nie została wylosowana. Jeśli tak, to powtórzy losowanie *//
-  const div = document.createElement("div"); //* Za każdym razem jak będzie wywołana funkcja, to się stworzy ten div *//
+  } 
+  const div = document.createElement("div");
   div.classList.add("blue-circle");
   div.textContent = wyniklosowania;
-  circles.appendChild(div); //* Wywołujemy tego diva *//
-  result.push(wyniklosowania); //* Dodaje uzyskane wyniki do tablicy "result"*//
+  circles.appendChild(div);
+  result.push(wyniklosowania);
 }
 
-//* Nie wywołujemy funkcji, ponieważ chcemy, żeby się wywołała po każdym kliknięciu a nie od razu *//
-
-//* Pobranie elementu, na którym pracujemy *//
-
 const button = document.querySelector("button");
-
-//* Ustawienie tego, że jeżeli ktoś kliknie, to wywoła się funkcja *//
 
 button.addEventListener("click", losowanie);
